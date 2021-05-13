0603faaba494601d4457cc5a37325ff0d2b8a4eb
Charlotte <48696244+summerysaturn@users.noreply.github.com>
2021-05-13 21:49:12 +0100

    feat(435): :sparkles::globe_with_meridians: added week 6 fully

--- 435-Web-Development/project-a2/tutorial-2-domains/ci435/sem2/session06/scripts/index.js
+++ 435-Web-Development/project-a2/tutorial-2-domains/ci435/sem2/session06/scripts/index.js
@@ -6,13 +6,18 @@ function main(val) {
     case 2:
       questionTwo(val);
       break;
+    case 3:
+      questionThree(val);
+      break;
+    case 4:
+      questionFour(val);
+      break;
+    case 5:
+      questionFive(val);
+      break;
     default:
       break;
   }
-}
-
-function mulValues(a, b) {
-  return a * b;
 }
 
 function questionOne(val) {
@@ -35,6 +40,14 @@ function questionOne(val) {
   );
 }
 
+function mulValues(a, b) {
+  return a * b;
+}
+
+function questionTwo(val) {
+  domainLog(val, `countChar("l", "hello"): ` + countChar("l", "hello"));
+}
+
 function countChar(c, s) {
   let counter = 0;
   for (let i = 0; i < s.length; i++) {
@@ -46,8 +59,92 @@ function countChar(c, s) {
   return counter;
 }
 
-function questionTwo(val) {
-  domainLog(val, `countChar("l", "hello"): ` + countChar("l", "hello"));
+function questionThree(val) {
+  domainLog(
+    val,
+    'countStr("cream", "I scream, you scream, we all scream for icecream!"): ' +
+      countStr("cream", "I scream, you scream, we all scream for icecream!")
+  );
+
+  domainLog(
+    val,
+    `countStr("sea", "She sells sea shells on the sea shore. The shells that she sells are sea shells I'm sure. So if she sells sea shells on the sea shore, I'm sure that the shells are sea shore shells"): ` +
+      countStr(
+        "sea",
+        "She sells sea shells on the sea shore. The shells that she sells are sea shells I'm sure. So if she sells sea shells on the sea shore, I'm sure that the shells are sea shore shells"
+      )
+  );
+}
+
+function countStr(val, str) {
+  let tempString = str;
+  let counter = 0;
+
+  while (true) {
+    firstPos = tempString.indexOf(val);
+
+    if (firstPos != -1) {
+      tempString = tempString.substring(firstPos + val.length);
+      counter++;
+    } else {
+      break;
+    }
+  }
+
+  return counter;
+}
+
+function questionFour(val) {
+  domainLog(
+    val,
+    `rotate(["Harry", "Sarah", "Oscar", "Tina"], 2): ` +
+      rotate(["Harry", "Sarah", "Oscar", "Tina"], 2)
+  );
+}
+
+function rotate(arr, num) {
+  for (let i = 0; i < num; i++) {
+    tempVal = arr.pop();
+    arr.unshift(tempVal);
+  }
+
+  return arr;
+}
+
+function questionFive(val) {
+  let arr = [12, 9, 33, 8, 71, 2, 41, 5];
+  domainLog(val, "doing a calculation (alphabetical)");
+  domainLog(
+    val,
+    "temp value = " +
+      (function () {
+        return arr.sort();
+      })()
+  );
+
+  domainLog(val, "doing a calculation (numerical)");
+  domainLog(
+    val,
+    "temp value = " +
+      (function () {
+        return arr.sort(function (a, b) {
+          return a - b;
+        });
+      })()
+  );
+
+  domainLog(val, "doing a calculation (reverse numerical)");
+  domainLog(
+    val,
+    "temp value = " +
+      (function () {
+        return arr
+          .sort(function (a, b) {
+            return a - b;
+          })
+          .reverse();
+      })()
+  );
 }
 
 function domainLog(destination, string) {
